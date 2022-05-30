1dc8639e51d30ac3f73eec7da86832a98f765a58
Andrey Mazurevich <andrey_mazurevich@epam.com>
2022-05-30 18:08:59 +0300

    Fixed color links

--- packages/dls/generated/js/color.tokens.js
+++ packages/dls/generated/js/color.tokens.js
@@ -1,19 +1,27 @@
 /**
  * Do not edit directly
- * Generated on Mon, 30 May 2022 14:32:12 GMT
+ * Generated on Mon, 30 May 2022 15:08:42 GMT
  */
 
-export const ColorBaseGrayLight = "#cccccc";
-export const ColorBaseGrayMedium = "#999999";
-export const ColorBaseGrayDark = "#111111";
-export const ColorBaseRed = "#ff0000";
-export const ColorBaseGreen = "#00ff00";
-export const CoreColorBasePrimaryBase = "#00bcd4";
-export const CoreColorBasePrimaryLight = "#b2ebf2";
-export const CoreColorBasePrimaryDark = "#0097a7";
-export const CoreColorBaseSecondaryBase = "#ff5722";
-export const CoreColorBaseSecondaryLight = "#ff9800";
-export const CoreColorBaseSecondaryDark = "#f44336";
+export const ColorPrimaryBackground = "#00bcd4";
+export const ColorPrimaryPaddingHorizontal = "#000000";
+export const ColorPrimaryPaddingVertical = "#000000";
+export const ColorPrimaryBorderRadius = "#000000";
+export const ColorPrimaryHoverBackground = "#0097a7";
+export const ColorOutlineBackground = "#000000";
+export const ColorOutlineBorderWidth = "#000000";
+export const ColorOutlineBorderColor = "#263238";
+export const ColorOutlineHoverBackground = "#e0e0e0";
+export const CoreColorPrimaryBase = "#00bcd4";
+export const CoreColorPrimaryLight = "#b2ebf2";
+export const CoreColorPrimaryDark = "#0097a7";
+export const CoreColorSecondaryBase = "#ff5722";
+export const CoreColorSecondaryLight = "#ff9800";
+export const CoreColorSecondaryDark = "#f44336";
+export const CoreColorDarkBase = "#263238";
+export const CoreColorDarkMild = "#37474f";
+export const CoreColorLightBase = "#f5f5f5";
+export const CoreColorLightMild = "#e0e0e0";
 export const SemanticColorError = "#EA0000";
 export const SemanticColorSuccess = "#009639";
 export const SemanticColorWarning = "#F77600";
